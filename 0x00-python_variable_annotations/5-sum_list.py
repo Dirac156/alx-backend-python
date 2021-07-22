7a12e542065d60b67a9b1e28096660c44c7608d6
= <=>
2021-07-22 17:23:36 +0200

    submit tasks

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains modules to perform sum of lists"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """return sum of list"""
+    return sum(input_list)
