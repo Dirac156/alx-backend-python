845b38a2a7b3395388a0264dd32bb7ba1887ab39
= <=>
2021-07-28 20:22:30 +0200

    submit tasks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ generate swapped list of float """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """generate return value of wait_random"""
     returned_value = [wait_random(max_delay) for i in range(n)]
     returned_value = asyncio.as_completed(returned_value)
