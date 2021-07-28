9e0afb99d419d4dd50dcece078d5ad0601c4992d
= <=>
2021-07-28 20:31:40 +0200

    submit tasks

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" measure coroutine """
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """ measure time execution of the function wait_n"""
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    elapsed_time = time.time() - start_time
+    return elapsed_time
