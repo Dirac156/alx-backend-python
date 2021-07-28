fef9a35ac36fb0eea57e2df3f1cccaa4bde328f2
= <=>
2021-07-28 20:08:05 +0200

    submit tasks

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" generate swapped list of float """
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    """generate return value of wait_random"""
+    
