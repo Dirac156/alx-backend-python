2696df6f1ed9a51030332e112a295e2360233c98
= <=>
2021-07-29 08:15:32 +0200

    submit tasks

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """ measure execution time"""
     start_time = time.perf_counter()
-    allTask = [async_comprehension for i in range(4)]
-    await asyncio.gather(*allTask)
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
     elapsed_time = time.perf_counter() - start_time
     return elapsed_time
